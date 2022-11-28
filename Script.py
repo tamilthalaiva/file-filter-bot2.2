937c0dc1ba02f42a6b53bc04607dc32f7e8fc4f7
tamilthalaiva <110980013+tamilthalaiva@users.noreply.github.com>
2022-11-28 21:45:42 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -2,26 +2,26 @@ class script(object):
     HELP_TXT = """𝙷𝙴𝚈 {}
 𝙷𝙴𝚁𝙴 𝙸𝚂 𝚃𝙷𝙴 𝙷𝙴𝙻𝙿 𝙵𝙾𝚁 𝙼𝚈 𝙲𝙾𝙼𝙼𝙰𝙽𝙳𝚂."""
     ABOUT_TXT = """<b>✯ 𝙼𝚈 𝙽𝙰𝙼𝙴: {}</b>
-✯ <b>𝙲𝚁𝙴𝙰𝚃𝙾𝚁: <a href=https://t.me/sahid_nalik>Sahid malik</a>
+✯ <b>𝙲𝚁𝙴𝙰𝚃𝙾𝚁: <a href=https://t.me/Tamil_Thalaiva>🆂🆄🆁🅹🅸🆃🅷</a>
 ✯ 𝙻𝙸𝙱𝚁𝙰𝚁𝚈: 𝙿𝚈𝚁𝙾𝙶𝚁𝙰𝙼
 ✯ 𝙻𝙰𝙽𝙶𝚄𝙰𝙶𝙴: 𝙿𝚈𝚃𝙷𝙾𝙽 𝟹
 ✯ 𝙳𝙰𝚃𝙰 𝙱𝙰𝚂𝙴: 𝙼𝙾𝙽𝙶𝙾 𝙳𝙱
 ✯ 𝙱𝙾𝚃 𝚂𝙴𝚁𝚅𝙴𝚁: 𝙷𝙴𝚁𝙾𝙺𝚄
 ✯ 𝙱𝚄𝙸𝙻𝙳 𝚂𝚃𝙰𝚃𝚄𝚂: v1.0.1 [ 𝙱𝙴𝚃𝙰 ]
-✯ updates channel: <a href=https://t.me/m_house786>CLICK HERE</a>
+✯ updates channel: <a href=https://t.me/Tamilthalaiva3>𝗧𝗮𝗺𝗶𝗹 𝗧𝗵𝗮𝗹𝗮𝗶𝘃𝗮</a>
 ✯ Subscribe you tube channel: <a href=<a href=https://youtube.com/channel/UCPaHDqWf3D3w2nxb8p3sr4A>CLICK HERE</a></b>""" 
     SOURCE_TXT = """<b>NOTE:</b>
-- <b>Movies house is a open source project. 
-- Source 👉 <a href=https://t.me/malik_bots/6>CLICK HERE</a></b>
+- <b>𝗦𝗕 is a open source project. 
+- Source 👉 <a href=https://t.me/Tamil_Thalaiva/6>CLICK HERE</a></b>
 
 <b>DEVS:</b>
-- <a href=https://t.me/sahid_malik>Sahid malik</a>"""
+- <a href=https://t.me/Tamil_Thalaiva>🆂🆄🆁🅹🅸🆃🅷</a>"""
     MANUELFILTER_TXT = """Help: <b>Filters</b>
 
 - Filter is the feature were users can set automated replies for a particular keyword and EvaMaria will respond whenever a keyword is found the message
 
 <b>NOTE:</b>
-1. movies house 🏠 have admin privillage.
+1. 𝗦𝗕 have admin privillage.
 2. only admins can add filters in a chat.
 3. alert buttons have a limit of 64 characters.
 
@@ -32,15 +32,15 @@ class script(object):
 • /delall - <code>delete the whole filters in a chat (chat owner only)</code>"""
     BUTTON_TXT = """Help: <b>Buttons</b>
 
-- movies house Supports both url and alert inline buttons.
+- 𝗦𝗕 Supports both url and alert inline buttons.
 
 <b>NOTE:</b>
 1. Telegram will not allows you to send buttons without any content, so content is mandatory.
-2. movies house supports buttons with any telegram media type.
+2. 𝗦𝗕 supports buttons with any telegram media type.
 3. Buttons should be properly parsed as markdown format
 
 <b>URL buttons:</b>
-<code>[Button Text](buttonurl:https://t.me/sahid_malik)</code>
+<code>[Button Text](buttonurl:https://t.me/Tamil_Thalaiva)</code>
 
 <b>Alert buttons:</b>
 <code>[Button Text](buttonalert:This is an alert message)</code>"""
@@ -96,7 +96,7 @@ This module only works for my admins
 ★ 𝚃𝙾𝚃𝙰𝙻 𝙲𝙷𝙰𝚃𝚂: <code>{}</code>
 ★ 𝚄𝚂𝙴𝙳 𝚂𝚃𝙾𝚁𝙰𝙶𝙴: <code>{}</code> 𝙼𝚒𝙱
 ★ 𝙵𝚁𝙴𝙴 𝚂𝚃𝙾𝚁𝙰𝙶𝙴: <code>{}</code> 𝙼𝚒𝙱
-★ 𝙲𝚁𝙴𝙰𝚃𝙾𝚁: <a href=https://t.me/sahid_nalik>Sahid malik</a></b> """
+★ 𝙲𝚁𝙴𝙰𝚃𝙾𝚁: <a href=https://t.me/Tamil_Thalaiva>𝗧𝗮𝗺𝗶𝗹 𝗧𝗵𝗮𝗹𝗮𝗶𝘃𝗮</a></b> """
     LOG_TEXT_G = """#NewGroup
 Group = {}(<code>{}</code>)
 Total Members = <code>{}</code>
@@ -114,4 +114,4 @@ I'm ⚡️ Powerful Auto-Filter Bot...
 Its Easy To Use Me; Just Add Me To Your Group As Admin, Thats All, i will Provide Movies There...😎
 ⚠️ More Help Check Help Button Below
 
-©️Mantained Bʏ  @sahid_malik</b>"""
+©️Mantained Bʏ  @Tamil_Thalaiva</b>"""
